6fa1aab0b4d2cbd72ab97e36eadcf3f06b703781
Seyi Oyemade <seyioyemade918@gmail.com>
2023-05-10 09:20:40 +0100

    Add reducers and actions

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+};
+
+export const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    checkStatus: (state) => {
+      state.status = 'Under construction';
+    },
+  },
+});
+
+export const { checkStatus } = categoriesSlice.actions;
+
+export default categoriesSlice.reducer;
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit';
+import booksReducer from './books/booksSlice';
+import categoriesReducer from './categories/categoriesSlice';
+
+export const store = configureStore({
+  reducer: {
+    books: booksReducer,
+    categories: categoriesReducer,
+  },
+});
+export default store;

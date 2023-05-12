5c9a3bb1157596204e7c88f3624556c8aa3232a4
Seyi Oyemade <seyioyemade918@gmail.com>
2023-05-12 18:41:36 +0100

    Add styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,24 @@
 import { Link } from 'react-router-dom';
+import { IoIosPerson } from 'react-icons/io';
 
 const Navbar = () => (
   <header>
-    <h1>Bookstore CMS</h1>
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">BOOKS</Link>
-        </li>
-        <li>
-          <Link to="/categories">CATEGORIES</Link>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <h1>Bookstore CMS</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">BOOKS</Link>
+          </li>
+          <li>
+            <Link to="/categories">CATEGORIES</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    <div>
+      <span className="user"><IoIosPerson /></span>
+    </div>
   </header>
 );
 
